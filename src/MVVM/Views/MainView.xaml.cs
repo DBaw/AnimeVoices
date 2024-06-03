8dce36190a7180978205035c64a2af364fc7362b
DBaw <dbawolek95@gmail.com>
2024-06-03 19:08:23 +0200

    Add manage app buttons

--- src/MVVM/Views/MainView.xaml.cs
+++ src/MVVM/Views/MainView.xaml.cs
@@ -1,16 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace AnimeVoices.MVVM.Views
 {
@@ -30,6 +19,16 @@ namespace AnimeVoices.MVVM.Views
 
             Application.Current.Resources.MergedDictionaries.RemoveAt(0);
             Application.Current.Resources.MergedDictionaries.Add(newTheme);
+        }
+
+        private void Exit_btn_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Minimize_btn_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
         }
     }
 }
