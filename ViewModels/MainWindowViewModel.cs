22353a24c23801e46b3742bc7bfc2789849790d1
DBaw <dbawolek95@gmail.com>
2024-12-15 16:34:43 +0100

    Create Avalonia project

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace AnimeVoices.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting { get; } = "Welcome to Avalonia!";
+    }
+}
