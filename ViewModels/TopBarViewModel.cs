56ba69b92094e0fb8b982e6483840e5de082e70f
DBaw <dbawolek95@gmail.com>
2024-12-24 14:26:34 +0100

    Add exit command

--- ViewModels/TopBarViewModel.cs
+++ ViewModels/TopBarViewModel.cs
@@ -1,8 +1,21 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Threading.Tasks;
 
 namespace AnimeVoices.ViewModels
 {
-    public class TopBarViewModel : ObservableObject
+    public partial class TopBarViewModel : ObservableObject
     {
+        public TopBarViewModel()
+        {
+            
+        }
+
+        [RelayCommand]
+        private async Task ExitApp()
+        {
+            Environment.Exit(0);
+        }
     }
 }
