b8e914ba38abc4c2566b81047edf6f583e139981
DBaw <dbawolek95@gmail.com>
2025-01-05 14:22:56 +0100

    Update properties

--- DataModels/DTOs/CharacterDto.cs
+++ DataModels/DTOs/CharacterDto.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using AnimeVoices.DataModels.DTOs.Nested;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace AnimeVoices.DataModels.DTOs
@@ -6,12 +9,20 @@ namespace AnimeVoices.DataModels.DTOs
     public class CharacterDto
     {
         [Key]
+        [JsonProperty("mal_id")]
         public int Id { get; set; }
-        [Required]
+
+        [JsonProperty("name")]
         public string Name { get; set; }
-        public string AnimeJson { get; set; }
-        public int Seiyuu { get; set; }
-        public string ImageUrl { get; set; }
+
+        [JsonProperty("about")]
+        public string About { get; set; }
+
+        [JsonProperty("images")]
+        public ImagesDto Images { get; set; }
+
+        [JsonProperty("voices")]
+        public List<VoiceDto> Voices { get; set; }
 
         public CharacterDto()
         {
---
+++ DataModels/DTOs/Nested/VoiceDto.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace AnimeVoices.DataModels.DTOs.Nested
+{
+    public class VoiceDto
+    {
+        [JsonProperty("language")]
+        public string Language { get; set; }
+
+        [JsonProperty("person")]
+        public SeiyuuDto Seiyuu { get; set; }
+    }
+}
