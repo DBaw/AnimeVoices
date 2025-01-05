b10f4c4ec1bcdb39cc56998265a357ee462839d4
DBaw <dbawolek95@gmail.com>
2025-01-05 23:33:52 +0100

    Refactor repository

--- DataAccess/Repositories/AnimeRepository.cs
+++ DataAccess/Repositories/AnimeRepository.cs
@@ -3,28 +3,52 @@ using AnimeVoices.DataAccess.Mappers;
 using AnimeVoices.DataModels.DTOs;
 using AnimeVoices.DataModels.Entities;
 using AnimeVoices.Models;
+using AnimeVoices.Stores;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace AnimeVoices.DataAccess.Repositories
 {
     public class AnimeRepository : IAnimeRepository
     {
-        public List<Anime> GetAllAnime(List<AnimeEntity> entities)
-        {
-            List<Anime> result = new();
-            
-            foreach (AnimeEntity entity in entities)
-            {
-                Anime anime = AnimeMapper.ToModel(entity);
-                result.Add(anime);
-            }
+        private readonly AnimeStore _animeStore;
+        private readonly IAnimeDatabase _animeDatabase;
 
-            return result;
+        public AnimeRepository(AnimeStore animeStore, IAnimeDatabase animeDatabase)
+        {
+            _animeStore = animeStore;
+            _animeDatabase = animeDatabase;
         }
 
-        public List<Anime> GetAnimeFromSeiyuu(SeiyuuDto dto)
+        public async Task InitializeAsync()
         {
-            return AnimeFactory.Create(dto);
+            var entities = await _animeDatabase.GetAllAnimeAsync();
+            var animeList = entities.Select(AnimeMapper.ToModel).ToList();
+
+            foreach (var anime in animeList)
+            {
+                _animeStore.Add(anime);
+            }
+        }
+
+        public void SaveAnimeFromSeiyuu(SeiyuuDto dto)
+        {
+            var animeList = AnimeFactory.Create(dto);
+
+            foreach (var anime in animeList)
+            {
+                // Add to store
+                _animeStore.Add(anime);
+
+                // Persist to database
+                _animeDatabase.SaveAnimeAsync(AnimeMapper.ToEntity(anime));
+            }
+        }
+
+        public List<Anime> GetAllAnime()
+        {
+            return _animeStore.AnimeCollection.ToList();
         }
     }
 }
