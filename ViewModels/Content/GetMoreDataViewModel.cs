e309a5ca395a7c10067e57dbf4d756d0741c3e44
DBaw <dbawolek95@gmail.com>
2025-01-27 14:56:52 +0100

    Setup more data page

--- ViewModels/Content/GetMoreDataViewModel.cs
+++ ViewModels/Content/GetMoreDataViewModel.cs
@@ -1,9 +1,26 @@
-﻿using CommunityToolkit.Mvvm.Messaging;
+﻿using AnimeVoices.ViewModels.Content.MoreData;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Messaging;
 
 namespace AnimeVoices.ViewModels.Content
 {
     public partial class GetMoreDataViewModel : BaseViewModel
     {
-        public GetMoreDataViewModel(IMessenger messenger) : base(messenger) { }
+        private readonly GetSingleDataViewModel _getSingleDataViewModel;
+        private readonly GetTopDataViewModel _getTopDataViewModel;
+
+        [ObservableProperty]
+        BaseViewModel _getSingleDataView;
+        [ObservableProperty]
+        BaseViewModel _getTopDataView;
+
+        public GetMoreDataViewModel(IMessenger messenger, GetSingleDataViewModel getSingleDataViewModel, GetTopDataViewModel getTopDataViewModel) : base(messenger)
+        {
+            _getSingleDataViewModel = getSingleDataViewModel;
+            _getTopDataViewModel = getTopDataViewModel;
+
+            GetSingleDataView = _getSingleDataViewModel;
+            GetTopDataView = _getTopDataViewModel;
+        }
     }
 }
